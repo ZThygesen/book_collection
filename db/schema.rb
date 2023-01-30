b49b9736a6190817268948a9b49cd823f90574f7
ZThygesen <zsthygesen@gmail.com>
2023-01-30 16:21:01 -0600

    generate model for books with more columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_171746) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_220636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
+    t.decimal "price"
+    t.date "publishedDate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
